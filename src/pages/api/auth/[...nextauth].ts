9a17cd3d0d2e4de914d5cad77ae873e97684bcbd
MorenaNobre <morenagnobre@gmail.com>
2022-01-10 12:40:05 -0300

    faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from "faunadb";
+
 import NextAuth, { Account, Profile, User } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -16,10 +20,19 @@ export default NextAuth({
     // ...add more providers here
   ],
   callbacks: {
-    async signIn({ user, account, profile, email, credentials }) {
-      return true
+    async signIn({ user, account, profile }) {
+      const {email} = user;
+      console.log(user);
+
+      try {
+        await fauna.query(q.Create(q.Collection("users"), { data: { email } }));
+        return true;
+      } catch (err) {
+        console.log(err);
+        return false;
+      }
     },
-  }
+  },
 });
 
 //FaunaDB create-update-delete - HTTP
--- src/services/fauna.ts
+++ src/services/fauna.ts
@@ -2,6 +2,7 @@ import { Client } from "faunadb";
 
 export const fauna = new Client({
   secret: process.env.FAUNADB_KEY,
+  domain: "db.fauna.com"
 });
 
 // Isso dará acesso ao banco de dados.
