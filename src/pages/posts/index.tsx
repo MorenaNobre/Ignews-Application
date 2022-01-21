7bef666d614095d918cfbb5185a8779541e5e936
MorenaNobre <morenagnobre@gmail.com>
2022-01-21 11:17:07 -0300

    Consuming Prismic API

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,4 +1,7 @@
+import { GetStaticProps } from "next";
 import Head from "next/head";
+import Prismic from "@prismicio/client";
+import { getPrismicClient } from "../../services/prismic";
 import styles from "./styles.module.scss";
 
 export default function Posts() {
@@ -13,20 +16,51 @@ export default function Posts() {
           <a href="#">
             <time>18 de janeiro de 2022</time>
             <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
-            <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process.</p>
+            <p>
+              In this guide, you will learn how to create a Monorepo to manage
+              multiple packages with a shared build, test, and release process.
+            </p>
           </a>
           <a href="#">
             <time>18 de janeiro de 2022</time>
             <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
-            <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process.</p>
+            <p>
+              In this guide, you will learn how to create a Monorepo to manage
+              multiple packages with a shared build, test, and release process.
+            </p>
           </a>
           <a href="#">
             <time>18 de janeiro de 2022</time>
             <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
-            <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process.</p>
+            <p>
+              In this guide, you will learn how to create a Monorepo to manage
+              multiple packages with a shared build, test, and release process.
+            </p>
           </a>
         </div>
       </main>
     </>
-  )
+  );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query(
+    [Prismic.predicates.at("document.type", "pubication")],
+    {
+      fetch: ["pubication.title", "pubication.content"],
+      pageSize: 100,
+    }
+  );
+
+  /**Quando debug com console.log e retorna um objeto ou array que está
+   * em cascata e não está aberto no console, então pode utilizar o JSON
+   * stringify como está ai abaixo.
+   */
+  console.log(JSON.stringify(response, null, 2));
+
+  return {
+    props: {},
+  };
+};
